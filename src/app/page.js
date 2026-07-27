898715b459214707365fabc6c35dc21c35e88188
Daniel GM <danielgarciamendoza.204@gmail.com>
2026-07-26 21:51:24 -0400

    Hero and Portfolio views added in the body

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,22 @@ import Navbar from "@/components/Navbar/Navbar";
 import ImageCard from "../components/ImageCard/ImageCard";
 import styles from "./page.module.css"; // Importamos tu CSS puro
 import Footer from "@/components/Footer/Footer";
+import Hero from "@/components/Hero/Hero";
+import PortfolioGallery from "@/components/PortfolioGallery/PortfolioGallery";
 
 export default function Inicio() {
   return (
     <main className={styles.main}>
+      {/* Header */}
       <Navbar />
-      <header className={styles.header}>
-        <h1 className={styles.mainTitle}>Barrancos Remodeling LLC</h1>
-        <p className={styles.subtitle}>
-          Explora nuestro portafolio de proyectos recientes. Nos especializamos
-          en transformar espacios y mantener exteriores en perfectas
-          condiciones.
-        </p>
-      </header>
 
-      <section className={styles.grid}>
-        <ImageCard
-          imageSrc="/images/cocina-terminada.jpg"
-          title="Remodelación de Cocina"
-          description="Instalación de gabinetes y encimeras modernas."
-        />
-        <ImageCard
-          imageSrc="/images/mantenimiento-cesped.jpg"
-          title="Mantenimiento de Césped"
-          description="Corte, perfilado y cuidado integral del jardín."
-        />
-      </section>
+      {/* Hero */}
+      <Hero />
+
+      {/* PortfolioGallery  */}
+      <PortfolioGallery />
+
+      {/* Footer */}
       <Footer />
     </main>
   );
