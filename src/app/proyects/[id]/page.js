8cb319dbed36a4ef3b5c61680764734b271d8799
Daniel GM <danielgarciamendoza.204@gmail.com>
2026-07-30 23:02:14 -0400

    List of images dinamic

--- src/app/proyects/[id]/page.js
+++ src/app/proyects/[id]/page.js
@@ -6,24 +6,18 @@ import styles from "./proyects.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import ProyectsDB from "../../../db/proyects.json";
+import { useRouter } from "next/navigation";
 
 export default function ProyectoDetalle({ params }) {
   // Find actual proyect
   const { id } = use(params);
   const ActualProyect = ProyectsDB.find((p) => p.link === id);
-  console.log("My proyect actual => ", ActualProyect);
+  // console.log("My proyect actual => ", ActualProyect);
 
   // Arreglo de imágenes gratuitas de Unsplash para pruebas
-  const imagenes = [
-    "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?auto=format&fit=crop&w=1200&q=80",
-    "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1600566753190-17f0baa2a6c3?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1600210492486-724fe5c67fb0?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1556912172-45b7ee98142a?auto=format&fit=crop&w=600&q=80",
-    "https://images.unsplash.com/photo-1484154218962-a197022b5858?auto=format&fit=crop&w=600&q=80",
-  ];
+  const imagenes = ActualProyect?.listOfImages
+    ? ActualProyect?.listOfImages
+    : [];
 
   // Estado para controlar qué imagen se está viendo en grande
   const [imagenActiva, setImagenActiva] = useState(0);
@@ -36,6 +30,8 @@ export default function ProyectoDetalle({ params }) {
   const anteriorImagen = () => {
     setImagenActiva((prev) => (prev === 0 ? imagenes.length - 1 : prev - 1));
   };
+
+  const router = useRouter();
 
   return (
     <main className={styles.mainContainer}>
@@ -92,58 +88,20 @@ export default function ProyectoDetalle({ params }) {
 
         {/* BARRA DE INFORMACIÓN (Cambia drásticamente de Móvil a PC en el CSS) */}
         <div className={styles.infoBar}>
-          <div className={styles.infoItem}>
-            <span className={styles.icon}>👤</span>
-            <div>
-              <div className={styles.infoLabel}>CLIENTE:</div>
-              <div className={styles.infoValue}>Privado</div>
-            </div>
-          </div>
-          <div className={styles.infoItem}>
-            <span className={styles.icon}>📍</span>
-            <div>
-              <div className={styles.infoLabel}>UBICACIÓN:</div>
-              <div className={styles.infoValue}>Atlanta, Georgia</div>
-            </div>
-          </div>
-          <div className={styles.infoItem}>
-            <span className={styles.icon}>🛁</span>
-            <div>
-              <div className={styles.infoLabel}>TIPO:</div>
-              <div className={styles.infoValue}>Baños & Cocinas</div>
-            </div>
-          </div>
-          <div className={styles.infoItem}>
-            <span className={styles.icon}>📅</span>
-            <div>
-              <div className={styles.infoLabel}>AÑO:</div>
-              <div className={styles.infoValue}>2026</div>
-            </div>
-          </div>
-          <div className={styles.infoItem}>
-            <span className={styles.icon}>📏</span>
-            <div>
-              <div className={styles.infoLabel}>ÁREA:</div>
-              <div className={styles.infoValue}>12 m²</div>
-            </div>
-          </div>
-        </div>
+          {/* DESCRIPCIÓN Y BOTÓN DE REGRESO */}
+          <div className={styles.descriptionSection}>
+            <p className={styles.descriptionText}>
+              {ActualProyect?.description}{" "}
+              <a href="#" className={styles.linkText}>
+                Agenda Una Visita Gratis Ahora.
+              </a>
+            </p>
 
-        {/* DESCRIPCIÓN Y BOTÓN DE REGRESO */}
-        <div className={styles.descriptionSection}>
-          <p className={styles.descriptionText}>
-            Un proyecto desafiante que transformó un espacio anticuado en un
-            refugio moderno y funcional, utilizando materiales de alta gama y
-            diseño personalizado.{" "}
-            <a href="#" className={styles.linkText}>
-              Ver más detalles sobre el proceso.
-            </a>
-          </p>
-
-          {/* Asumiendo que tu página principal es "/" o "/galeria" */}
-          <Link href="/" className={styles.backButton}>
-            REGRESAR AL PORTAFOLIO &#10230;
-          </Link>
+            {/* Asumiendo que tu página principal es "/" */}
+            <button onClick={() => router.back()} className={styles.backButton}>
+              REGRESAR AL PORTAFOLIO &#10230;
+            </button>
+          </div>
         </div>
       </div>
       <Footer />
