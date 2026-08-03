44f3a2c2a8f561b13bc8d8d917a003c5cc3002ec
Daniel GM <danielgarciamendoza.204@gmail.com>
2026-08-03 00:03:25 -0400

    Contact button added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,13 +7,17 @@ import { usePathname } from "next/navigation";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  // Estado para controlar si el menú de celular está abierto o cerrado
+  // Estado para controlar el menú principal en celular
   const [menuAbierto, setMenuAbierto] = useState(false);
-  // Obtenemos la ruta actual para saber qué link subrayar
+  // Nuevo estado para controlar el submenú de Contacto
+  const [contactoAbierto, setContactoAbierto] = useState(false);
+
   const pathname = usePathname();
 
   const toggleMenu = () => {
     setMenuAbierto(!menuAbierto);
+    // Si cerramos el menú de celular, cerramos también el de contacto
+    if (menuAbierto) setContactoAbierto(false);
   };
 
   return (
@@ -54,26 +58,37 @@ export default function Navbar() {
         <ul
           className={`${styles.navLinks} ${menuAbierto ? styles.menuActivo : ""}`}
         >
+          {/* Home */}
           <li>
             <Link
               href="/"
               className={pathname === "/" ? styles.linkActivo : styles.link}
-              onClick={() => setMenuAbierto(false)}
+              onClick={() => {
+                setMenuAbierto(false);
+                setContactoAbierto(false);
+              }}
             >
               Inicio
             </Link>
           </li>
+
+          {/* About us */}
           <li>
             <Link
               href="/#nosotros"
               className={
                 pathname === "/#nosotros" ? styles.linkActivo : styles.link
               }
-              onClick={() => setMenuAbierto(false)}
+              onClick={() => {
+                setMenuAbierto(false);
+                setContactoAbierto(false);
+              }}
             >
               Nosotros
             </Link>
           </li>
+
+          {/* Services */}
           <li>
             <Link
               href="/#servicesGallery"
@@ -82,14 +97,26 @@ export default function Navbar() {
                   ? styles.linkActivo
                   : styles.link
               }
-              onClick={() => setMenuAbierto(false)}
+              onClick={() => {
+                setMenuAbierto(false);
+                setContactoAbierto(false);
+              }}
             >
               Servicios
-              {/* Icono de flechita hacia abajo imitando tu diseño */}
+            </Link>
+          </li>
+
+          {/* Contact */}
+          <li className={styles.contactoWrapper}>
+            <button
+              className={styles.linkBoton}
+              onClick={() => setContactoAbierto(!contactoAbierto)}
+            >
+              Contactar
               <svg
-                className={styles.flecha}
-                width="10"
-                height="6"
+                className={`${styles.flecha} ${contactoAbierto ? styles.flechaRotada : ""}`}
+                width="12"
+                height="8"
                 viewBox="0 0 10 6"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -102,19 +129,64 @@ export default function Navbar() {
                   strokeLinejoin="round"
                 />
               </svg>
-            </Link>
-          </li>
+            </button>
 
-          <li>
-            <Link
-              href="/#formulario"
-              className={
-                pathname === "/#formulario" ? styles.linkActivo : styles.link
-              }
-              onClick={() => setMenuAbierto(false)}
-            >
-              Contacto
-            </Link>
+            {/* Menú Desplegable Animado */}
+            {contactoAbierto && (
+              <div className={styles.dropdownContacto}>
+                <a
+                  href="tel:+15123440698"
+                  className={styles.dropdownItem}
+                  onClick={() => {
+                    setContactoAbierto(false);
+                    setMenuAbierto(false);
+                  }}
+                >
+                  <div className={styles.iconoFondo}>
+                    {/* Ícono de Teléfono Profesional */}
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                    </svg>
+                  </div>
+                  <span>Llamar ahora</span>
+                </a>
+
+                <a
+                  href="sms:+15123440698?body=Quiero%20un%20estimado%20para%20"
+                  className={styles.dropdownItem}
+                  onClick={() => {
+                    setContactoAbierto(false);
+                    setMenuAbierto(false);
+                  }}
+                >
+                  <div className={styles.iconoFondo}>
+                    {/* Ícono de Mensaje Profesional */}
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+                    </svg>
+                  </div>
+                  <span>Mandar mensaje</span>
+                </a>
+              </div>
+            )}
           </li>
         </ul>
       </div>
